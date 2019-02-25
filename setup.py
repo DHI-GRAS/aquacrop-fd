ddc5b6a3c7216069244ec57089302fe9aefb99a7
Jonas Sølvsteen <j08lue@gmail.com>
2019-02-25 21:00:29 +0100

    travis

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='aquacrop',
+    version='0.1.0',
     description='AquaCrop wrapper',
     url='https://github.com/DHI-GRAS/aquacrop-fd',
     author='Jonas Solvsteen',
     author_email='josl@dhigroup.com',
     install_requires=[
-        'xarray',
-        'rasterio'
+        'xarray'
     ],
+    extras_require={
+        'test': [
+            'pytest'
+        ]
+    },
     packages=find_packages()
 )
