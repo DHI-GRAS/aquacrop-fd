c9fa6cfaa8fb5dd534fe345bc8c22b507835d77e
Jonas Sølvsteen <j08lue@gmail.com>
2019-03-21 15:12:54 +0100

    more queues

--- aquacrop_fd/queue_interface.py
+++ aquacrop_fd/queue_interface.py
@@ -1,7 +1,24 @@
 import requests
 
+from aquacrop_fd import interface
+from aquacrop_fd import queue_schemas
+
 
 def get_job(api_url):
+    return {
+        'fraction': 80.0,
+        'geometry': {'type': 'Rectangle',
+                     'coordinates': [[[-3.6035156249999996, 10.708109848387668],
+                                      [-3.6035156249999996, 11.82864282964517],
+                                      [-2.5048828125, 11.82864282964517],
+                                      [-2.5048828125, 10.708109848387668]]]},
+        'area_name': 'Volta',
+        'guid': '9cc4dd76-7417-446a-8245-26412322c907',
+        'planting_date': '2019-01-01',
+        'crop': 'Maize',
+        'irrigated': False
+    }
+
     url = api_url + '/getjob'
     r = requests.post(url, json={'num_messages': 1})
     if not r.ok:
@@ -14,9 +31,25 @@ def get_job(api_url):
     return r.json()
 
 
-def work_queue(api_url):
+def work_queue(
+        api_url,
+        plu_path, eto_path, tmp_min_path, tmp_max_path,
+        soil_map_path, land_cover_path
+):
+    if not api_url.endswith('/'):
+        api_url = api_url + '/'
 
     while True:
         job = get_job(api_url)
         if job is None:
             break
+
+        schema = queue_schemas.JobSchema()
+        jobkw = schema.load(job)
+
+        interface(
+            plu_path=plu_path, eto_path=eto_path,
+            tmp_min_path=tmp_min_path, tmp_max_path=tmp_max_path,
+            soil_map_path=soil_map_path, land_cover_path=land_cover_path,
+            **jobkw
+        )
